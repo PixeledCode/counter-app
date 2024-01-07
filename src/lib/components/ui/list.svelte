b61ceb3db0e632e174e6bde4ee5824b25295cc21
pixeledcode <ashoaib540918@gmail.com>
2024-01-07 22:23:43 +0530

    add transition on count change

--- src/lib/components/ui/list.svelte
+++ src/lib/components/ui/list.svelte
@@ -2,18 +2,73 @@
 	import { AddCount, Button } from '$lib/components';
 	import store from '$lib/counter.store';
 	import { CandyOff, Minus, Plus } from 'lucide-svelte';
+	import gsap from 'gsap';
 
 	function changeCount(name: string, type: 'add' | 'reduce') {
-		const newArr = [...$store].map((item) => {
+		const newArr = [...$store].map((item, i) => {
 			if (item.name === name) {
+				let newCount;
+				if (type === 'reduce') {
+					prevTransition(i);
+					newCount = Number(item.count) - 1;
+				} else {
+					nextTransition(i);
+					newCount = Number(item.count) + 1;
+				}
+
 				return {
 					...item,
-					count: type === 'reduce' ? Number(item.count) - 1 : Number(item.count) + 1
+					count: newCount
 				};
 			}
 			return item;
 		});
 		store.editList(newArr);
+	}
+
+	let temp = [...$store];
+
+	function prevTransition(index: number) {
+		let prevNode = document.querySelector(`#prev-count-${index}`) as HTMLElement;
+		let currentNode = document.querySelector(`#current-count-${index}`) as HTMLElement;
+		let nextNode = document.querySelector(`#next-count-${index}`) as HTMLElement;
+		gsap.to([prevNode, currentNode], {
+			duration: 0.3,
+			y: -28,
+			ease: 'power2.inOut',
+			onComplete: () => {
+				nextNode.innerHTML = currentNode.innerHTML;
+				currentNode.innerHTML = prevNode.innerHTML;
+				gsap.set([prevNode, currentNode], { y: 0 });
+				prevNode.innerHTML = String(Number(prevNode.innerHTML) - 1);
+			}
+		});
+	}
+
+	function nextTransition(index: number) {
+		let prevNode = document.querySelector(`#prev-count-${index}`) as HTMLElement;
+		let currentNode = document.querySelector(`#current-count-${index}`) as HTMLElement;
+		let nextNode = document.querySelector(`#next-count-${index}`) as HTMLElement;
+		gsap.to([nextNode, currentNode], {
+			duration: 0.3,
+			y: 28,
+			ease: 'power2.inOut',
+			onComplete: () => {
+				prevNode.innerHTML = currentNode.innerHTML;
+				currentNode.innerHTML = nextNode.innerHTML;
+				gsap.set([nextNode, currentNode], { y: 0 });
+				nextNode.innerHTML = String(Number(nextNode.innerHTML) + 1);
+			}
+		});
+	}
+
+	function replaceClass(
+		node: { classList: { remove: (arg0: any) => void; add: (arg0: any) => void } },
+		oldClass: string,
+		newClass: string
+	) {
+		node.classList.remove(oldClass);
+		node.classList.add(newClass);
 	}
 </script>
 
@@ -25,14 +80,21 @@
 	</section>
 {:else}
 	<section class="flex flex-col gap-6 mt-4">
-		{#each $store as { name, count }}
+		{#each $store as { name, count }, i}
 			<article class="flex flex-col gap-1">
 				<h2 class="text-xl font-semibold tracking-tight">{name}</h2>
 				<div class="flex justify-between items-center gap-2">
 					<Button variant="outline" size="icon" on:click={() => changeCount(name, 'reduce')}
 						><Minus /></Button
 					>
-					<span class="text-xl font-semibold"> {count} </span>
+					<div>
+						<div class="flex flex-col items-center justify-center h-10 overflow-clip">
+							<span class="text-xl font-semibold" id={`next-count-${i}`}>{temp[i].count + 1}</span>
+							<span class="text-xl font-semibold" id={`current-count-${i}`}>{temp[i].count}</span>
+							<span class="text-xl font-semibold" id={`prev-count-${i}`}>{temp[i].count - 1} </span>
+						</div>
+						<span class="text-xl font-semibold sr-only"> {count} </span>
+					</div>
 					<Button variant="outline" size="icon" on:click={() => changeCount(name, 'add')}
 						><Plus /></Button
 					>
